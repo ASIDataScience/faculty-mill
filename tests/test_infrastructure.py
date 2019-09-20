a4ece2b9b5fb9071d75b5cc2457b495bef917536
Víctor Zabalza <vzabalza@gmail.com>
2019-09-20 10:26:30 +0100

    Fix flake8 errors in tests

--- tests/test_infrastructure.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-
-def test_that_importing_versions_works():
-    from faculty_mill.version import version
---
+++ tests/test_version.py
@@ -0,0 +1,2 @@
+def test_that_importing_versions_works():
+    from faculty_mill.version import version  # noqa
